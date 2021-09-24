4aa108f16276f759c78239cfff05feeba6ef57ad
Alex <alexbachurin91@gmail.com>
2021-09-24 18:44:22 +0300

    fetch random person from api and transform its return

--- src/App.js
+++ src/App.js
@@ -2,12 +2,46 @@ import React, { useEffect, useState } from "react";
 const defaultImage = 'https://res.cloudinary.com/dljezd6qv/image/upload/v1631577257/react-birthday-reminder/avatar-placeholder.png'
 function App() {
 
+  //state for storing person after fetch
+  const [person, setPerson] = useState(null)
 
   //display property of data attribute on every mouse over
   const handleMouseOver = (e) => {
     console.log(e.target.dataset.property)
   }
 
+  //fetch person, we getting array with single object as response in data.results
+  const fetchPersons = async () => {
+    const response = await fetch('https://randomuser.me/api/');
+    const data = await response.json();
+    console.log(data.results);
+    //transform result to good looking object
+    const newPerson = data.results.map(item => {
+      const { email, phone } = item;
+      const { age } = item.dob;
+      const { number, name } = item.location.street;
+      const streetName = `${number} ${name}`;
+      const { password } = item.login;
+      const { first, last } = item.name;
+      const fullName = `${first} ${last}`;
+      return {
+        fullName,
+        email,
+        phone,
+        age,
+        streetName,
+        password
+      }
+    })
+    console.log(newPerson)
+    setPerson(newPerson[0])
+  }
+  useEffect(() => {
+    fetchPersons();
+
+  }, [])
+
+  console.log(person)
   return (
     <main>
       <div className="block bcg-black">
@@ -18,12 +52,12 @@ function App() {
           <p className="user-title">title</p>
           <p className="user-value">value</p>
           <div className="values-list">
-            <button className="icon" data-property="name" onMouseOver={handleMouseOver}>1</button>
-            <button className="icon" data-property="email" onMouseOver={handleMouseOver}>2</button>
-            <button className="icon" data-property="age" onMouseOver={handleMouseOver}>3</button>
-            <button className="icon" data-property="street" onMouseOver={handleMouseOver}>4</button>
-            <button className="icon" data-property="phone" onMouseOver={handleMouseOver}>5</button>
-            <button className="icon" data-property="password" onMouseOver={handleMouseOver}>6</button>
+            <button className="icon" data-property="name" onMouseOver={handleMouseOver}>name</button>
+            <button className="icon" data-property="email" onMouseOver={handleMouseOver}>email</button>
+            <button className="icon" data-property="age" onMouseOver={handleMouseOver}>age</button>
+            <button className="icon" data-property="street" onMouseOver={handleMouseOver}>street</button>
+            <button className="icon" data-property="phone" onMouseOver={handleMouseOver}>phone</button>
+            <button className="icon" data-property="password" onMouseOver={handleMouseOver}>password</button>
           </div>
           <button className="btn">random</button>
         </div>
